d8e90fef8585c9e9bbfa0493ee592fcee6537ede
Muhammad Choirulanam <mchoiranam@gmail.com>
2024-10-05 23:33:05 +0700

    profile

---
+++ lib/components/reusable_textfield.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ReusableTextField extends StatelessWidget {
+  final String hintText;
+  final TextEditingController controller;
+  final bool readOnly;
+
+  const ReusableTextField({
+    required this.hintText,
+    required this.controller,
+    this.readOnly = false,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      readOnly: readOnly,
+      decoration: InputDecoration(
+        hintText: hintText.isEmpty ? null : hintText, // Menampilkan hintText jika tidak kosong
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        contentPadding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      ),
+    );
+  }
+}
--- lib/pages/menus/profile.dart
+++ lib/pages/menus/profile.dart
@@ -1,12 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:getx_apps/components/reusable_card.dart';
+import 'package:getx_apps/components/reusable_textfield.dart';
 
 class Profile extends StatelessWidget {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'User Profile',
-        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+        automaticallyImplyLeading: false, // Hapus panah kembali
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ReusableTextField(
+                hintText: 'Nama Lengkap',
+                controller: nameController,
+              ),
+              SizedBox(height: 16),
+              ReusableTextField(
+                hintText: 'Email',
+                controller: emailController,
+              ),
+              SizedBox(height: 16),
+              ReusableTextField(
+                hintText: 'Nomor Telepon',
+                controller: phoneController,
+              ),
+              SizedBox(height: 20),
+              ReusableCard(
+                imageAsset: 'assets/images/fritzy.jpeg',
+                title: 'Judul',
+                subtitle: 'Deskripsi',
+                price: 'Harga',
+                isHorizontal: false,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
